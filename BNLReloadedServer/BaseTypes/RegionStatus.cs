324dd67a9ef921a29ec90b711f4b119931bdab1a
devprbtt <121635649+devprbtt@users.noreply.github.com>
2025-12-26 07:48:07 -0300

    Include hero selection in active game status

--- BNLReloadedServer/BaseTypes/RegionStatus.cs
+++ BNLReloadedServer/BaseTypes/RegionStatus.cs
@@ -10,7 +10,9 @@ public class RegionStatus
     public int Port { get; set; }
     public int OnlinePlayers { get; set; }
     public Dictionary<string, int> Queues { get; set; } = new();
+    public Dictionary<string, List<StatusQueuePlayer>> QueuePlayers { get; set; } = new();
     public List<StatusCustomGame> CustomGames { get; set; } = [];
+    public List<StatusGameStatus> ActiveGames { get; set; } = [];
     public string? Error { get; set; }
 
     public static void WriteRecord(BinaryWriter writer, RegionStatus value)
@@ -21,7 +23,9 @@ public class RegionStatus
         writer.Write(value.Port);
         writer.Write(value.OnlinePlayers);
         writer.WriteMap(value.Queues, writer.Write, writer.Write);
+        writer.WriteMap(value.QueuePlayers, writer.Write, (writer, players) => writer.WriteList(players, StatusQueuePlayer.WriteRecord));
         writer.WriteList(value.CustomGames, StatusCustomGame.WriteRecord);
+        writer.WriteList(value.ActiveGames, StatusGameStatus.WriteRecord);
         writer.WriteOption(value.Error, writer.Write);
     }
 
@@ -35,7 +39,10 @@ public class RegionStatus
             Port = reader.ReadInt32(),
             OnlinePlayers = reader.ReadInt32(),
             Queues = reader.ReadMap<string, int, Dictionary<string, int>>(reader.ReadString, reader.ReadInt32),
+            QueuePlayers = reader.ReadMap<string, List<StatusQueuePlayer>, Dictionary<string, List<StatusQueuePlayer>>>(reader.ReadString,
+                () => reader.ReadList<StatusQueuePlayer, List<StatusQueuePlayer>>(StatusQueuePlayer.ReadRecord)),
             CustomGames = reader.ReadList<StatusCustomGame, List<StatusCustomGame>>(StatusCustomGame.ReadRecord),
+            ActiveGames = reader.ReadList<StatusGameStatus, List<StatusGameStatus>>(StatusGameStatus.ReadRecord),
             Error = reader.ReadOption(reader.ReadString)
         };
     }
